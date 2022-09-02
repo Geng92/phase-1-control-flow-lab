09ba883f82c13653af4024a6b856f9795ca3097e
Geng-Wung <gengxis92@gmail.com>
2022-09-02 11:53:10 -0400

    commit tree daa49f31d6f2698110996c7110a97febf4e69461

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue <= 400) {
+    return 'This one is on me!'
+  }
+  else if (someValue <= 2000) {
+    return 'That will be twenty bucks.'
+  }
+  else if (someValue >= 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else (someValue >= 2500) 
+    return 'No can do.'
+  }
+  
+
+
+function ternaryCheckCity(NYC){
+  return NYC ==='NYC'?"Ok, sounds good." : 'No go.';
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  switch (Tip){
+    case 'generous':
+      return "Thank you so much." 
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break; 
+    default: 
+      return "Bye."
+  }
 }
